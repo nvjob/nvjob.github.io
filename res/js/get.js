70440e16b8f5dad5f4c7977fbbd07a248769261b
#NVJOB Nicholas Veselov <reset86@gmail.com>
2019-10-13 14:42:16 +0300

    Update get.js

--- res/js/get.js
+++ res/js/get.js
@@ -2,8 +2,7 @@ $(document).ready(function() {
 
 var get = window.location.search.replace('?','').split('&');
 get = get.reduce(function(p,e){ var a = e.split('='); p[ decodeURIComponent(a[0])] = decodeURIComponent(a[1]); return p;},{});
-var nameurl = get['url'];
-nameurl = nameurl.replace(/^https?:\/\//,'');
+var nameurl = get['url'].replace(/^https?:\/\//,'');
 nameurl = nameurl.replace(/^http?:\/\//,'');
 document.getElementById('urlgen').innerHTML=('<a class="follink" href="')+get['url']+('">')+nameurl+('</a>');
 
